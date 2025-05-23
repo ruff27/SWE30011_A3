48a120e7c018ceb3632709ddf7127fc834b1e617
Ruffin <110576761+ruff27@users.noreply.github.com>
2025-05-23 12:08:16 +1000

    init 1

--- sketch_mar6a/A1/A1.ino
+++ sketch_mar6a/A1/A1.ino
@@ -1,49 +1,37 @@
-#define RELAY_PIN 8
-#define LED_RED   9
-#define LED_GREEN 10
-#define LED_BLUE  11
-#define BUZZER    12
+#include <DHT.h>
+
+#define DHTPIN 3
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+const int pirPin = 2;     // PIR motion sensor
+const int lightPin = A1;  // Phototransistor analog pin
 
 void setup() {
   Serial.begin(115200);
-  pinMode(RELAY_PIN, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
-
-  // Initialize all to OFF
-  digitalWrite(RELAY_PIN, LOW);
-  digitalWrite(LED_RED, LOW);
-  digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_BLUE, LOW);
-  digitalWrite(BUZZER, LOW);
+  dht.begin();
+  pinMode(pirPin, INPUT);
 }
 
 void loop() {
-  if (Serial.available()) {
-    String command = Serial.readStringUntil('\n');
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  int lightValue = analogRead(lightPin);
+  int motionDetected = digitalRead(pirPin);
 
-    if (command.startsWith("FAN:")) {
-      if (command.endsWith("ON")) {
-        digitalWrite(RELAY_PIN, HIGH);
-      } else if (command.endsWith("OFF")) {
-        digitalWrite(RELAY_PIN, LOW);
-      }
-    } else if (command.startsWith("LED:")) {
-      // Format: LED:R,G,B (0 or 1)
-      int r = command.charAt(4) - '0';
-      int g = command.charAt(6) - '0';
-      int b = command.charAt(8) - '0';
-      digitalWrite(LED_RED, r);
-      digitalWrite(LED_GREEN, g);
-      digitalWrite(LED_BLUE, b);
-    } else if (command.startsWith("BUZZER:")) {
-      if (command.endsWith("ON")) {
-        digitalWrite(BUZZER, HIGH);
-      } else if (command.endsWith("OFF")) {
-        digitalWrite(BUZZER, LOW);
-      }
-    }
+  // Only send if readings are valid
+  if (!isnan(temperature) && !isnan(humidity)) {
+    Serial.print("TEMP:");
+    Serial.print(temperature);
+    Serial.print(",HUMIDITY:");
+    Serial.print(humidity);
+    Serial.print(",LIGHT:");
+    Serial.print(lightValue);
+    Serial.print(",MOTION:");
+    Serial.println(motionDetected);
+  } else {
+    Serial.println("ERROR: Failed to read from DHT22");
   }
+
+  delay(2000);  // Wait 2 seconds between readings
 }
